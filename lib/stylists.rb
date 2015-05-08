dc12aea7e7f6b35c5b059868e5c1880004c14168
John Young <jayelwhy@yahoo.com>
2015-05-08 09:47:46 -0700

    include save() for Stylists

--- lib/stylists.rb
+++ lib/stylists.rb
@@ -10,10 +10,15 @@ class Stylists
     returned_stylists = DB.exec("SELECT * FROM stylists;")
     stylists = []
     returned_stylists.each() do |stylist|
-      name = stylist.fetch("name")
-      id = stylist.fetch("id").to_i()
-      stylists.push(Stylist.new({:name => name, :id => id}))
+      name = stylists.fetch("name")
+      id = stylists.fetch("id").to_i()
+      stylists.push(Stylists.new({:name => name, :id => id}))
     end
     stylists
     end
+
+    define_method(:save) do
+    result = DB.exec("INSERT INTO stylists (stylist) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
 end
--- spec/stylists_spec.rb
+++ spec/stylists_spec.rb
@@ -2,9 +2,25 @@ require('spec_helper')
 
 
 describe(Stylists) do
+
   describe(".all") do
     it("starts off with no Stylists") do
       expect(Stylists.all()).to(eq([]))
+    end
+  end
+
+    describe("#name") do
+    it("tells you their name") do
+      stylist = Stylists.new({:name => "Debbie", :id => nil})
+      expect(stylist.name()).to(eq("Debbie"))
+    end
+  end
+
+  describe("#id") do
+    it("sets its ID when you save them") do
+      stylist = Stylists.new({:name => "Debbie", :id => nil})
+      stylist.save()
+      expect(stylist.id()).to(be_an_instance_of(Fixnum))
     end
   end
 end
